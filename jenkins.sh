dd5deea503b900e0fff5adb8e28cbdb8caf57518
Your Name <you@example.com>
2025-02-15 20:09:35 +0530

    msg

--- jenkins.sh
+++ jenkins.sh
@@ -4,20 +4,13 @@ rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum install fontconfig java-17-openjdk jenkins -y
 
 #resize disk from 20GB to 50GB
-lsblk
-df -hT
-name=$(lsblk -dn -o NAME | head -n 1)
-growpart /dev/$name 4
-# growpart /dev/nvme0n1 4
-df -hT
+growpart /dev/nvme0n1 4
 
-# resize logical volumes
-lvextend -L +10G /dev/mapper/RootVG-homeVol
+lvextend -L +10G /dev/RootVG/rootVol
 lvextend -L +10G /dev/mapper/RootVG-varVol
 lvextend -l +100%FREE /dev/mapper/RootVG-varTmpVol
 
-# resize filesystems
-xfs_growfs /home
+xfs_growfs /
 xfs_growfs /var/tmp
 xfs_growfs /var
 
